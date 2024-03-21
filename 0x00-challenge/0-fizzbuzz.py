a1b0326c79223dd91877fc3b88e1a092bb92ebbc
mathewkoech <mathewkoech55@gmail.com>
2024-03-21 11:26:00 +0300

    fix fizzbuzz

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -19,14 +19,14 @@ def fizzbuzz(n):
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
             tmp_result.append("FizzBuzz")
-        if (i % 3) == 0:
+        elif (i % 3) == 0:
             tmp_result.append("Fizz")
-        
         elif (i % 5) == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
     print(" ".join(tmp_result))
+
 
 
 if __name__ == '__main__':
